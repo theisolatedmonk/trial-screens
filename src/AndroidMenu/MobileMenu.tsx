b90b6bfbdce8ca9b3154645822a2d0400b3077b5
theisolatedmonk <96rahulseth@gmail.com>
2023-05-07 16:57:59 +0530

    .

---
+++ src/AndroidMenu/MobileMenu.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import ellips from "@/images/Ellipse 704.png";
+import Image from "next/image";
+
+import {
+  DeleteSVG,
+  HelpSVG,
+  InboxSVG,
+  NotificationsSvg,
+  SearchSVG,
+} from "./MobileMenuSVG";
+
+export  function MobileMenu() {
+  return (
+    <div className="flex flex-col w-full gap-4   font-Figtree  ">
+      <div className="flex justify-end">
+      <DeleteSVG />
+      </div>
+      <div className="flex gap-2 items-center px-4">
+        <Image src={ellips} alt={""} />
+        <div className="flex flex-col  ">
+          <p className=" font-bold">Jane Smith</p>
+          <p className="">Sales Executive</p>
+        </div>
+      </div>
+      <hr className="w-full" />
+      <div className=" flex gap-2 items-center px-4">
+        <SearchSVG />
+        <p>Search</p>
+      </div>
+      <hr className="w-full" />
+      <div className=" flex gap-2 items-center px-4">
+        <InboxSVG />
+        <p>Inbox</p>
+      </div>
+      <hr className="w-full" />
+      <div className=" flex gap-2 items-center px-4">
+        <NotificationsSvg />
+        <p>Notifications</p>
+      </div>
+      <hr className="w-full" />
+      <div className=" flex gap-2 items-center px-4">
+        <HelpSVG />
+        <p>Help</p>
+      </div>
+      <hr className="w-full" />
+    </div>
+  );
+}
+
