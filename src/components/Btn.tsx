8510f8461d80203357ed33328887e0a6f2cfd6a5
theisolatedmonk <96rahulseth@gmail.com>
2023-05-08 16:48:37 +0530

    key is added to Btn

--- src/components/Btn.tsx
+++ src/components/Btn.tsx
@@ -3,26 +3,28 @@ import React from "react";
 import { Button, Space } from "antd";
 const headerLinks = [
   { link: "Basic Info", active: true },
-  { link: "File", active:false  },
+  { link: "File", active: false },
   { link: "Mutual Action Plan", active: false },
   { link: "Contact", active: false },
-  
 ];
 
 export const Btn: React.FC = () => (
   <div className=" flex text-[#BDC3C2]  justify-center sm:justify-between">
     <Space size={[8, 16]}>
-    {headerLinks.map(({ link, active }) => (
-      //   <Button className={`rounded-b-none`}>{link}</Button>
-      <Button
-        className={`rounded-b-none font-bold text-sm sm:text-lg ${
-          active ? "text-[#00B686]" : "text-[#BDC3C2] "
-        }`}
-      >
-        {link}
-      </Button>
-    ))}
-  </Space>
-  <Button className="hidden text-[#BDC3C2] text-lg font-bold sm:flex text-center">Internal Actions</Button>
+      {headerLinks.map(({ link, active }, i) => (
+        //   <Button className={`rounded-b-none`}>{link}</Button>
+        <Button
+          key={i}
+          className={`rounded-b-none font-bold text-sm sm:text-lg ${
+            active ? "text-[#00B686]" : "text-[#BDC3C2] "
+          }`}
+        >
+          {link}
+        </Button>
+      ))}
+    </Space>
+    <Button className="hidden text-[#BDC3C2] text-lg font-bold sm:flex text-center">
+      Internal Actions
+    </Button>
   </div>
 );
